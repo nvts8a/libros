d99797edaa2d08410124112e13018a216702d385
Steven Justin <steven@edovo.com>
2023-05-10 10:21:40 -0500

    WIP

---
+++ src/app/utilities/Epub2Txt.cpp
@@ -0,0 +1,4 @@
+#include "Epub2Txt.h"
+#include "uzlib.h"
+
+Epub2Txt::Epub2Txt() {}
---
+++ src/app/utilities/Epub2Txt.h
@@ -0,0 +1,19 @@
+#ifndef Epub2Txt_h
+#define Epub2Txt_h
+#include "uzlib.h"
+
+class Epub2Txt {
+public:
+    static bool extract() {
+
+        return true;
+    }
+
+protected:
+
+private:
+    Epub2Txt();
+};
+
+
+#endif // Epub2Txt_h
